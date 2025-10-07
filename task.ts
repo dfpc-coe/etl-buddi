15a91ce3f4f8d6c0dd2ce01d118dc053c6576bb2
ingalls <nick@ingalls.ca>
2025-10-07 13:35:12 -0600

    Setup MVP requests

--- task.ts
+++ task.ts
@@ -1,10 +1,12 @@
 import { Static, Type, TSchema } from '@sinclair/typebox';
 import type { Event } from '@tak-ps/etl';
+import { parse } from 'date-fns';
 import { Feature } from '@tak-ps/node-cot'
 import ETL, { SchemaType, handler as internal, local, DataFlowType, InvocationType } from '@tak-ps/etl';
 import { fetch } from '@tak-ps/etl';
 
 // https://eagle-preprod.buddi.co.uk/index.html?action=help#help
+// https://eagle-preprod.buddi.co.uk/apidocs/
 const InputSchema = Type.Object({
     CustomerID: Type.String({
         description: 'The Customer ID provided by Buddi'
@@ -15,13 +17,33 @@ const InputSchema = Type.Object({
     ClientSecret: Type.String({
         description: 'The Client Secret provided by Buddi'
     }),
-    'DEBUG': Type.Boolean({
+    MonitoredOnly: Type.Boolean({
+        default: true,
+        description: 'If true, only return monitored devices'
+    }),
+    Timeframe: Type.String({
+        default: 'Last Day',
+        enum: ['All', 'Last Day', 'Last 7 Days'],
+    }),
+    DEBUG: Type.Boolean({
         default: false,
         description: 'Print results in logs'
     })
 });
 
-const OutputSchema = Type.Object({})
+const OutputSchema = Type.Object({
+    wearerId: Type.Integer(),
+    firstName: Type.String(),
+    lastName: Type.String(),
+    lastGPSTime: Type.Union([Type.String(), Type.Null()]),
+    strapStatus: Type.Integer(),
+    onCharge: Type.Integer(),
+    gpsSignal: Type.Union([Type.Integer(), Type.Null()]),
+    batteryPercentage: Type.Union([Type.Integer(), Type.Null()]),
+    latitude: Type.Union([Type.Number(), Type.Null()]),
+    longitude: Type.Union([Type.Number(), Type.Null()]),
+    locationAddress: Type.Union([Type.String(), Type.Null()])
+});
 
 export default class Task extends ETL {
     static name = 'etl-buddi'
@@ -59,9 +81,122 @@ export default class Task extends ETL {
             }
         });
 
-        await authRes.json();
+        const token = await authRes.typed(Type.Object({
+            access_token: Type.String(),
+            expires_at_utc: Type.String(),
+            token_type: Type.String()
+        }), {
+            verbose: env.DEBUG || false
+        });
+
+        const trackerMap = new Map<string, Static<typeof Feature.InputFeature>>();
+
+        let page = 1;
+        let pages;
+
+        do {
+            const locURL = new URL(base + '/v1/wearers/locations');
+            locURL.searchParams.set('page', String(page));
+            locURL.searchParams.set('per_page', String(100))
+            if (env.MonitoredOnly) {
+                locURL.searchParams.set('monitored_only', 'true');
+            }
+
+            if (env.Timeframe && env.Timeframe !== 'All') {
+                const date = new Date();
+                if (env.Timeframe === 'Last Day') {
+                    date.setDate(date.getDate() - 1);
+                } else if (env.Timeframe === 'Last 7 Days') {
+                    date.setDate(date.getDate() - 7);
+                }
+
+                locURL.searchParams.set('start_date', date.toISOString().split('T')[0]);
+            }
+
+            const locRes = await fetch(locURL, {
+                method: 'GET',
+                headers: {
+                    Accept: 'application/json',
+                    Authorization: `Buddi-oauthtoken: ${token.access_token}`,
+                    'User-Agent': 'CloudTAK ETL/1.0',
+                },
+            });
+
+            const trackers = await locRes.typed(Type.Object({
+                result: Type.Integer(),
+                data: Type.Optional(Type.Array(OutputSchema)),
+                meta: Type.Optional(Type.Object({
+                    total: Type.Integer(),
+                    page: Type.Integer(),
+                    per_page: Type.Integer(),
+                    pages: Type.Integer()
+                })),
+                global_server_type: Type.Optional(Type.String()),
+                global_server_code: Type.Optional(Type.String())
+            }), {
+                verbose: env.DEBUG || false
+            });
+
+            if (trackers.data) {
+                for (const tracker of trackers.data) {
+                    if (!tracker.lastGPSTime || !tracker.latitude || !tracker.longitude) {
+                        continue;
+                    }
+
+                    const id = `buddi-${tracker.wearerId}`;
+                    const start = parse(tracker.lastGPSTime, 'MM/dd/yyyy hh:mm:ssaa', new Date());
+
+                    const existing = trackerMap.get(id);
+
+                    if (existing && new Date(existing.properties.start) > start) {
+                        continue;
+                    }
+
+                    const stale = new Date();
+                    stale.setMinutes(stale.getMinutes() + 2);
+
+                    trackerMap.set(id, {
+                        id,
+                        type: 'Feature',
+                        properties: {
+                            callsign: `Buddi: ${tracker.firstName} ${tracker.lastName}`,
+                            type: 'a-s-G',
+                            how: 'm-g',
+                            start,
+                            time: new Date().toISOString(),
+                            stale: stale.toISOString(),
+                            status: {
+                                battery: String(tracker.batteryPercentage || 0),
+                            },
+                            metadata: tracker
+                        },
+                        geometry: {
+                            type: 'Point',
+                            coordinates: [tracker.longitude, tracker.latitude]
+                        }
+                    });
+                }
+            } else {
+                console.log('no more data found, exiting paging');
+                break;
+            }
+
+            if (!pages && trackers.meta.pages) {
+                pages = trackers.meta.pages;
+            }
+
+            if (!pages) {
+                console.log('could not determine number of pages');
+                break;
+            }
+
+            ++page;
+        } while (page <= pages);
 
         const features: Static<typeof Feature.InputFeature>[] = [];
+        for (const feature of trackerMap.values()) {
+            features.push(feature);
+        }
 
         const fc: Static<typeof Feature.InputFeatureCollection> = {
             type: 'FeatureCollection',
@@ -72,7 +207,7 @@ export default class Task extends ETL {
     }
 }
 
-await local(new Task(import.meta.url), import.meta.url);
+await local(await Task.init(import.meta.url), import.meta.url);
 export async function handler(event: Event = {}) {
     return await internal(new Task(import.meta.url), event);
 }
